209f2023184af6d5f780be4ae1be7a78e27f6552
Justin Abrahms <justin@abrah.ms>
2015-07-07 21:50:48 -0700

    `python setup.py test` now works.

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from setuptools import setup, find_packages, Command
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
-class PyTest(Command):
-    user_options = []
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
     def initialize_options(self):
-        pass
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
-        import subprocess
-        import sys
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
